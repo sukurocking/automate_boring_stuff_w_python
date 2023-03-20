78dbfb4e1be8953a1fb5f4fc1d21f21e8ae3ab1b
Sukumar Subudhi <suku.subudhi@gmail.com>
2023-03-20 21:06:42 +0530

    working with strings and modified readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# automate_boring_stuff_w_python
+# Automate_boring_stuff_w_python
+This github repo is for practising the projects in "Automate Boring Stuff with Python book"
+On a journey to become a better programmer.
+
---
+++ working_with_strings.py
@@ -0,0 +1,16 @@
+# Join method
+', '.join(["cats", "rats", "bats"])
+" ".join(["My", "name", "is", "Simon"])
+
+# Split method
+"My name is Simon".split(" ")
+
+spam = '''Dear Alice,
+How have you been? I am fine. There is a container in the fridge that is labeled "Milk Experiment."
+Please do not drink it.
+Sincerely,
+Bob'''
+spam.split("\n")
+
+# partition method
+'Hello, world!'.partition('w')
